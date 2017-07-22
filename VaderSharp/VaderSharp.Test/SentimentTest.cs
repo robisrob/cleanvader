10aaa6a330ee0487134b373a273d95a2fefd6937
robs <rob.swartenbroekx@cegeka.com>
2017-07-22 11:18:29 +0200

    added fluentassertions

--- VaderSharp/VaderSharp.Test/SentimentTest.cs
+++ VaderSharp/VaderSharp.Test/SentimentTest.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using VaderSharp.Domain;
+using FluentAssertions;
 
 namespace VaderSharp.Test
 {
@@ -12,24 +13,24 @@ namespace VaderSharp.Test
             SentimentIntensityAnalyzer analyzer = new SentimentIntensityAnalyzer();
 
             var standardGoodTest = analyzer.PolarityScores("VADER is smart, handsome, and funny.");
-            Assert.Equal(standardGoodTest.Negative, 0);
-            Assert.Equal(standardGoodTest.Neutral, 0.254);
-            Assert.Equal(standardGoodTest.Positive, 0.746);
-            Assert.Equal(standardGoodTest.Compound, 0.8316);
+            standardGoodTest.Negative.Should().Be(0);
+            standardGoodTest.Neutral.Should().Be(0.254);
+            standardGoodTest.Positive.Should().Be(0.746);
+            standardGoodTest.Compound.Should().Be(0.8316);
 
             var kindOfTest = analyzer.PolarityScores("The book was kind of good.");
-            Assert.Equal(kindOfTest.Negative, 0);
-            Assert.Equal(kindOfTest.Neutral, 0.657);
-            Assert.Equal(kindOfTest.Positive, 0.343);
-            Assert.Equal(kindOfTest.Compound, 0.3832);
+            kindOfTest.Negative.Should().Be(0);
+            kindOfTest.Neutral.Should().Be(0.657);
+            kindOfTest.Positive.Should().Be(0.343);
+            kindOfTest.Compound.Should().Be(0.3832);
 
             var complexTest =
                 analyzer.PolarityScores(
                     "The plot was good, but the characters are uncompelling and the dialog is not great.");
-            Assert.Equal(complexTest.Negative, 0.327);
-            Assert.Equal(complexTest.Neutral, 0.579);
-            Assert.Equal(complexTest.Positive, 0.094);
-            Assert.Equal(complexTest.Compound, -0.7042);
+            complexTest.Negative.Should().Be(0.327);
+            complexTest.Neutral.Should().Be(0.579);
+            complexTest.Positive.Should().Be(0.094);
+            complexTest.Compound.Should().Be(-0.7042);
         }
     }
 }
